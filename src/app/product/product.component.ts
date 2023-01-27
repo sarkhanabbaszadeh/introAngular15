ea11a042cdd3d2b580776e43fc5a5d492292b1ae
Sarkhan Abbaszadeh <s.abbaszada@ady.az>
2023-01-27 13:19:48 +0400

    fixed image,symbl

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -13,6 +13,6 @@ title="Product List"
 products:Product[]=[
   {id:1,name:"Asus Laptop",price:2500,categoryId:1,description:"This Laptop is Perfect !",imageUrl:"https://images.unsplash.com/photo-1630794180018-433d915c34ac?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1032&q=80"},
   {id:2,name:"Toshiba Laptop",price:1500,categoryId:1,description:"This Laptop is Not Bad !",imageUrl:"https://images.unsplash.com/photo-1542744095-291d1f67b221?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=870&q=80"},
-  {id:3,name:"Logitech Mouse",price:1500,categoryId:2,description:"This Mouse is So Beautifully !",imageUrl:"https://images.unsplash.com/photo-1498050108023-c5249f4df085?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=872&q=80"},
+  {id:3,name:"Logitech Mouse",price:1500,categoryId:2,description:"This Mouse is So Beautifully !",imageUrl:"https://images.unsplash.com/photo-1672636836550-01093fe3d208?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80"},
 ]
 }
