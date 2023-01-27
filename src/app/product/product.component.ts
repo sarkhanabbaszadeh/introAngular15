f0d9d33da052d2a5b036917912599c4873765e76
Sarkhan Abbaszadeh <s.abbaszada@ady.az>
2023-01-27 12:01:53 +0400

    add components

--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -7,4 +7,10 @@ import { Component } from '@angular/core';
 })
 export class ProductComponent {
 name="Laptop Product"
+title="Product List"
+products:any[]=[
+  {id:1,name:"Asus Laptop",price:2500,categoryId:1,Description:"This Laptop is Perfect !"},
+  {id:2,name:"Toshiba Laptop",price:1500,categoryId:1,Description:"This Laptop is Not Bad !"},
+  {id:3,name:"Logitech Mouse",price:1500,categoryId:2,Description:"This Mouse is So Beautifully !"},
+]
 }
